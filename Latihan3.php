a2ba8bd25017c857d8e63730ac8a5a337e1a6b0c
unknown <muhammad_rafly_27rpl@student.smktelkom-mlg.sch.id>
2019-02-20 14:11:07 +0700

    sampun

---
+++ Latihan3.php
@@ -0,0 +1,18 @@
+<?php  
+    $a=3;  
+    $b=2; 
+ 
+    echo $a."<br/>";  //a = 3 akan tampil 3  
+    $a += 1;              //a Berisi 3 + 1   
+    echo $a."<br/>";  
+    $a -= $b;             //a Berisi 4 - 2   
+    echo $a."<br/>";  
+    $a *= $b;             //a Berisi 2 x 2   
+    echo $a."<br/>";  
+    $a /= $b;             //a Berisi 4 / 2   
+    echo $a."<br/>";  
+    $a .= $b;             //a Berisi 2.2   
+    echo $a."<br/>";  
+    $a %= $b;             //a Berisi 22 % 2   
+    echo $a."<br/>"; 
+?>
